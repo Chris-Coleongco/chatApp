627db6b4211afd270c4c6920f2c8df296d1835fe
chris-coleongco <christien.coleongco@gmail.com>
2023-10-21 21:12:49 -0600

    hehehe

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,13 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Auth } from "./components/auth";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './components/home';
 
 function App() {
 
+
+
   return (
-    <div className="App">
-      <Auth />
-    </div>
+    <>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
+    </>
+    
   )
 }
 export default App
---
+++ vite-project/src/components/home.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+export const Home = () => {
+
+    return (
+        <>
+        <h1>dis is hom pag</h1>
+
+        <a href='login'>click here to log in</a>
+        </>
+    );
+}
